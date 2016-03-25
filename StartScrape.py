42fd477403a16e927c8a164590438da98a0390d4
pkwv <bnuzpk@163.com>
2016-03-25 23:55:09 +0800

    start thread of scrape proxy and baike

---
+++ StartScrape.py
@@ -0,0 +1,43 @@
+# -*- coding:utf8 -*- 
+
+from ScrapeBaike import ScrapeBaike 
+from ScrapeProxy import ScrapeProxy
+import time
+import threading
+
+URL_START = 5000000
+URL_END = 6500000
+THREAD_NUM = 8
+
+def print_time():
+    print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+def main():
+    scr_p = ScrapeProxy('Scrape Proxy',100 ,60)
+    scr_p.start()
+    time.sleep(20)
+
+    st = URL_START
+    ed = URL_END
+    thread_num = THREAD_NUM
+    num = (ed - st) / thread_num
+    i = 0
+    j = st
+    thread_l=[]
+    while i<thread_num:
+        thread_l.append(ScrapeBaike('scraper_'+str(i), j, j+num))
+        j += num
+        i += 1
+    for k in thread_l:
+        k.start()
+    while threading.activeCount()>1:
+        print_time()
+        for k in thread_l:
+            k.print_success_error()
+        time.sleep(200)
+    for k in thread_l:
+        k.print_msg(str(k.cnt) + 'error: ' + str(k.error))
+
+
+if __name__ == '__main__':
+    main()
