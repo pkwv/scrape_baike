637bcb3452e5f4c3db37a5d5c82e9be6ab75cec6
pkwv <bnuzpk@163.com>
2016-03-26 16:22:51 +0800

    separated from StartScrape.py must run before StartScrape

---
+++ StartProxy.py
@@ -0,0 +1,16 @@
+# -*- coding:utf8 -*-
+
+from ScrapeProxy import ScrapeProxy
+import threading
+import time
+
+
+def main():
+    scrapeproxy = ScrapeProxy('scrape proxy', 100, 100)
+    scrapeproxy.start()
+    print 'lalal'
+
+#print __name__
+
+if __name__ == '__main__':
+    main()
