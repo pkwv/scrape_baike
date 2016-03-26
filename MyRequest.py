79ec6454c3db7ecfec9f0bee042089c99591ad0d
pkwv <bnuzpk@163.com>
2016-03-26 12:03:54 +0800

    use random shuffle to select proxy

--- MyRequest.py
+++ MyRequest.py
@@ -8,39 +8,47 @@ import requests
 import random 
 
 def MyRequest(url, UseProxy = True):
-    conn = ConnMysql.ConnMysql()
-    cur = conn.cursor()
-    cur.execute("select * from proxies")
-    res = cur.fetchall()
-    l = cur.rowcount
-    cur.close()
-    conn.close()
-    r = len(user_agent.USER_AGENTS)
-    cnt = 0
     while True:
-        cnt += 1
+        conn = ConnMysql.ConnMysql()
+        cur = conn.cursor()
+        cur.execute("select * from proxies")
+        res = cur.fetchall()
+        l = cur.rowcount
+        cur.close()
+        conn.close()
+        lst = []
+        for i in res:
+            lst.append(i[0])
+        random.shuffle(lst)
+        r = len(user_agent.USER_AGENTS)
         if UseProxy:
-            i = random.randint(0, l - 1)
-            proxies = {'http':res[i][0]}
-        j = random.randint(0, r - 1)
-        headers = {'user-agent':user_agent.USER_AGENTS[j]}
-        try:
-            if UseProxy:
-                #print headers
-                #print proxies
-                req = requests.get(url, headers=headers, proxies=proxies, timeout=(3.05,27))
-            else:
-                req = requests.get(url,headers=headers)
-        except requests.exceptions.RequestException as e:
-           # print e 
-            conn = ConnMysql.ConnMysql()
-            cur = conn.cursor()
-            if UseProxy:
-                cur.execute("delete from proxies where url = %s",(res[i],))
-            conn.commit()
-            cur.close()
-            conn.close()
-            continue
-        print url + ' cnt  ' + str(cnt)
-        return req
-
+            #i = random.randint(0, l - 1)
+            #proxies = {'http':res[i][0]}
+            cnt = 0
+            for i in lst:
+                cnt += 1
+                proxies = {'http':i}
+                j = random.randint(0, r - 1)
+                headers = {'user-agent':user_agent.USER_AGENTS[j]}
+                try:
+                    req = requests.get(url, headers=headers, proxies=proxies, timeout=(3.05,60))
+                except requests.exceptions.RequestException as e:
+                   # print e 
+                    conn = ConnMysql.ConnMysql()
+                    cur = conn.cursor()
+                    cur.execute("delete from proxies where url = %s",(i,))
+                    conn.commit()
+                    cur.close()
+                    conn.close()
+                    continue
+                print url + ' cnt  ' + str(cnt)
+                return req
+        else:
+           
+            j = random.randint(0, r - 1)
+            headers = {'user-agent':user_agent.USER_AGENTS[j]}
+            try:
+                req = requests.get(url, headers=headers)
+            except requests.exceptions.RequestException as e:
+                print e
+            return req
