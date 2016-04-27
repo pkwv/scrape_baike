276fda0e90039439b10d8f254a7350338107c24c
pkwv <bnuzpk@163.com>
2016-04-27 10:43:28 +0800

    check before xpath && keep trying untill insert into mysql

--- ScrapeBaike.py
+++ ScrapeBaike.py
@@ -30,69 +30,122 @@ class ScrapeBaike(threading.Thread):
         self.success = 0
         self.error = 0
         self.f=open('log/'+str(thread_id)+'.txt','w')
-        self.text= ''
+        self.url=''
+        self.web=''
+        self.notuse=0
+        self.use = 0
+        self.proxy = ''
+        self.error_page=[]
+    def get_proxy(self):
+        conn = ConnMysql.ConnMysql()
+        cur = conn.cursor()
+        cur.execute('select * from proxies limit 1')
+        res = cur.fetchone()
+        self.proxy = res[0][0]
+        cur.execute('delete from proxies where url=%s',(self.proxy,))
+        conn.commit()
+        cur.close()
+        conn.close()
     def print_msg(self, content):
         print str(self.thread_id) + ' ' + content
     
     def print_success_error(self):
         print '~~~~' + str(self.thread_id) + ' success ' + str(self.success) + ' error ' + str(self.error)
-    
-    def make_request(self, url):
-        return  MyRequest.MyRequest(url)
+
+    def make_request(self, url, use = True):
+        if not use:
+            self.notuse += 1
+        else:
+            self.use += 1
+        self.url = url
+        return  MyRequest.MyRequest(url, use, self.thread_id)
  
     def get_unicode_text(self, res):
         if res.encoding.lower() == err_code:
-            return res.text.encode(err_code).decode(requests.utils.get_encodings_from_content(res.text)[0])
+            lst = requests.utils.get_encodings_from_content(res.text)
+            if(len(lst)==0):
+                return res.text.encode(err_code).decode('utf-8')
+            else:
+                return res.text.encode(err_code).decode(lst[0])
+            #return res.text.encode(err_code).decode(requests.utils.get_encodings_from_content(res.text)[0])
         else:
             return res.text
-    def add_error(self, url):
-        self.error += 1
-        url = self.get_url(url)
-        conn = self.CONN()
-        cur = conn.cursor()
-        cur.execute("select 1 from error_page where url = %s",(url,))
-        res = cur.fetchone()
-        if not res:
-            #already in error
-            #maybe can not visit here!
-            cur.execute("insert into error_page (url) values(%s)",(url,))
-        conn.commit()
-        cur.close()
-        conn.close()
+    def check(self):
+        if self.web == '':
+            return False
+        try:
+            sel = etree.HTML(self.get_unicode_text(self.web))
+        except:
+            self.f.write(self.url+' '+self.web.url + ' error in check\n')
+            #self.print_msg('here')
+            return False
+       
+        #print item['url']
+        word=self.link_list(sel.xpath('//dd[@class="lemmaWgt-lemmaTitle-title"]/h1/text()'))  #utf8
+        if word=='':
+            return False
+        return True
+        #self.print_msg(res.url + '  ' + word)
+        #explanation=self.link_list(sel.xpath('string(//div[@class="lemma-summary"])'))   #utf8
+        
+
     def run(self):
         i = self.st
         while i < self.ed:
             ''' just do it'''
             url = 'http://baike.baidu.com/view/' + str(i) + '.htm'
-            res = self.make_request(url)
-            if 'error' in res.url:
-                #self.add_error(url)
-                self.f.write(url + ' error in url\n')
-                #print 'error in url'
+            ok = False
+            req_num = 3
+            #has = False
+            for j in range(req_num):
+                # the last request, i will not use proxy
+                self.web = self.make_request(url, j!=req_num-1)
+                # it must be error in baike
+                #if 'baidu' not in self.web.url:# or '127.0.0.1' in self.web.url or 'baidu' not in self.web.url:
+                #    if j!=req_num-1:
+                #        has = True
+                if self.check():
+                    self.parse_item()
+                    self.parse()
+                    ok = True
+                    break
+            #if ok and has:
+            #    self.f.write('rule is not right\n')
+            if not ok:
+                if self.web=='':
+                    url_out = 'redirect'
+                else :
+                    url_out = self.web.url
+                self.f.write(self.url + ' '+url_out+' error in url\n')
                 self.error += 1
-            else:
-                self.parse(res)
             i += 1
-            #t = random.uniform(0.05,0.35)
-            #time.sleep(t)
             if i%1000 == 0:
                 self.print_msg(str(i))
+                self.f.flush()
+        print str(self.thread_id)+' is finished'
 
-    def parse(self, res):
+    def parse(self):
+        #self.parse_item(res)
         # url do not contain 'error' 
-        if 'subview' in res.url:
-            sel = etree.HTML(self.get_unicode_text(res))
+        if 'subview' in self.web.url:
+            sel = etree.HTML(self.get_unicode_text(self.web))
             suburl = sel.xpath('//ul[@class="polysemantList-wrapper cmn-clearfix"]//li/a/@href')
             for url in suburl:
                 u = 'http://baike.baidu.com'+url
-                r = self.make_request(u)
-                if 'error' in r.url:
+                ok = False
+                req_num = 3
+                for i in range(req_num):
+                    # the last request, i will not use proxy
+                    self.web = self.make_request(u,i!=req_num-1)
+                    if self.check():
+                        self.parse_item()
+                        ok = True
+                        break
+                if not ok:
                     #self.add_error(url)
-                    self.f.write(url + ' error in suburl\n')
+                    self.f.write(self.url + ' ' + self.web.url + ' error in suburl\n')
                     self.error += 1
-                else:
-                    self.parse_item(r)
-        self.parse_item(res)
+        #self.parse_item(res)
     def get_url(self, url):
         res = ''
         for i in url:
@@ -119,17 +172,17 @@ class ScrapeBaike(threading.Thread):
         return  res
     def CONN(self):
         return ConnMysql.ConnMysql() 
-    def parse_item(self, res):
+    def parse_item(self):
         # res.url don't have 'error'
         #print self.get_unicode_text(res).encode('utf-8')
         try:
-            sel = etree.HTML(self.get_unicode_text(res))
+            sel = etree.HTML(self.get_unicode_text(self.web))
         except:
-            self.f.write(res.url + ' error in etree.HTML\n')
+            self.f.write(self.url+' '+self.web.url + ' error in etree.HTML\n')
             self.error += 1
             #self.print_msg('here')
             return
-        url = self.get_url(res.url)
+        url = self.get_url(self.web.url)
         urlmd5id = md5(url).hexdigest()
         now = datetime.utcnow().replace(microsecond=0).isoformat(' ')
 
@@ -148,7 +201,7 @@ class ScrapeBaike(threading.Thread):
         #self.print_msg(res.url + '  ' + word)
         explanation=self.link_list(sel.xpath('string(//div[@class="lemma-summary"])'))   #utf8
         if word == '':
-            self.f.write(res.url + ' no word' + '\n')
+            self.f.write(self.url+' '+self.web.url + ' no word' + '\n')
             self.error += 1
             return 
         l1=sel.xpath('//div[@class="basic-info cmn-clearfix"]//dl//dt//text()')
@@ -161,7 +214,10 @@ class ScrapeBaike(threading.Thread):
                 if l2[j] != '\n':
                     break;
                 j += 1
-            dict[i]=l2[j]
+            if j<l2_len:
+                dict[i]=l2[j]
+            else:
+                break
         #item['summarize']=json.dump(dict)   # json
         summarize=json.JSONEncoder().encode(dict)
         full_text=self.link_list(sel.xpath('//div[contains(concat(" ", @class, " "), " para-title ")]//text() |'
@@ -186,25 +242,30 @@ class ScrapeBaike(threading.Thread):
         cur.execute(
             "select 1 from baikeword_%s where urlmd5id  = %s",(ind,urlmd5id,))
         ret = cur.fetchone()
+        ok = False
+        while not ok:
+            try:
+                if ret:
+                    cur.execute("""
+                        update baikeword_%s set title = %s,photo=%s,word=%s,explanation=%s,summarize=%s,
+                        full_text=%s,collect=%s,like_num=%s,share=%s,reference=%s,tag=%s,time=%s,author=%s,
+                        view=%s,now=%s,id=%s where urlmd5id=%s
+                        """,(ind,title,photo,word,explanation,
+                             summarize,full_text,collect,like,share,
+                             reference,tag,time,author,
+                             view,now,wordid,urlmd5id)
+                    )
+                else:
+                    cur.execute("""
+                    insert into baikeword_%s(urlmd5id,title,photo,word,explanation,summarize,full_text,collect,like_num,share,
+                    reference,tag,time,author,view,now,id)
+                    values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
+                    """,(ind,urlmd5id,title,photo,word,explanation,summarize,full_text,collect,like,share,reference,tag,time,author,view,now,wordid))
 
-        if ret:
-            cur.execute("""
-                update baikeword_%s set title = %s,photo=%s,word=%s,explanation=%s,summarize=%s,
-                full_text=%s,collect=%s,like_num=%s,share=%s,reference=%s,tag=%s,time=%s,author=%s,
-                view=%s,now=%s,id=%s where urlmd5id=%s
-                """,(ind,title,photo,word,explanation,
-                     summarize,full_text,collect,like,share,
-                     reference,tag,time,author,
-                     view,now,wordid,urlmd5id)
-            )
-        else:
-            cur.execute("""
-            insert into baikeword_%s(urlmd5id,title,photo,word,explanation,summarize,full_text,collect,like_num,share,
-            reference,tag,time,author,view,now,id)
-            values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
-            """,(ind,urlmd5id,title,photo,word,explanation,summarize,full_text,collect,like,share,reference,tag,time,author,view,now,wordid))
-
-        conn.commit()
+                conn.commit()
+                ok = True
+            except:
+                time.sleep(60*1)
         cur.close()
         conn.close()
         self.success += 1
