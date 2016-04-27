9ab8fb915c20f719ef69f20f46373c7a0377c2ba
pkwv <bnuzpk@163.com>
2016-04-27 10:49:11 +0800

    can use command to run this code

--- StartScrape.py
+++ StartScrape.py
@@ -4,10 +4,12 @@ from ScrapeBaike import ScrapeBaike
 from ScrapeProxy import ScrapeProxy
 import time
 import threading
+import sys
 
-URL_START = 0
-URL_END = 500000
-THREAD_NUM = 50
+# default 
+URL_START = 8000000
+URL_END = 9000000
+THREAD_NUM = 20
 
 def print_time():
     print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
@@ -32,14 +34,41 @@ def main():
         k.start()
     while threading.activeCount()>1:
         print_time()
+        from_my_ip = 0
+        from_proxy = 0
+        success = 0
         for k in thread_l:
             k.print_success_error()
-        time.sleep(100)
+            from_my_ip += k.notuse
+            from_proxy += k.use
+            success += k.success
+        print 'use my ip: ' + str(from_my_ip) + ' and ' + str(from_proxy) + ' from proxy'
+        print 'there are: ' + str(success) + ' success'
+        print 'there are '+ str(threading.activeCount()-1)+' spiders still scraping'
+        time.sleep(120)
     for k in thread_l:
         k.join();
+    tot = 0
+    suc = 0
+    err = 0
     for k in thread_l:
         k.print_success_error() #msg(str(k.success) + '  error: ' + str(k.error))
+        tot += k.notuse
+        suc += k.success
+        err += k.error
+    print 'use my ip: ' + str(tot)
+    print 'total scrape success: '+str(suc)
+    print 'error number: ' + str(err)
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print 'you must input 4 parameters\n 0:filename 1:start url 2:end url 3:number of thread'
+        sys.exit()
+    URL_START = int(sys.argv[1])
+    URL_END = int(sys.argv[2])
+    THREAD_NUM = int(sys.argv[3])
+    st_time=time.time()
     main()
+    end_time=time.time()
+    print 'total cost time: ' + str(end_time-st_time)
